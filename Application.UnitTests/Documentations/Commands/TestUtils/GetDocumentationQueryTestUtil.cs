f017e37cdf263a92dda429af0cf602470d63820f
Theagns <markus.agnsgaard@hotmail.com>
2024-05-28 13:33:30 +0200

    changes

---
+++ Application.UnitTests/Documentations/Commands/TestUtils/GetDocumentationQueryTestUtil.cs
@@ -0,0 +1,12 @@
+namespace EngrafoCA.Application.UnitTests.Documentations.Commands.TestUtils
+{
+  public class GetDocumentationQueryTestUtil
+  {
+    public static CreateQuery()
+    {
+
+
+      return GetDocumentationQuery();
+    }
+  }
+}

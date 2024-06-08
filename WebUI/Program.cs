ff6aaefdd57e772ad03a8828dff6205838db58c8
Rasmus Dalgaard <rasmus.dalgaard@knowit.dk>
2024-06-08 18:20:29 +0200

    add logging middleware

--- WebUI/Program.cs
+++ WebUI/Program.cs
@@ -1,14 +1,15 @@
-using Application;
-using Infrastructure;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
+using WebUI.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//! Add services to the container.
-builder.Services
-	.AddApplication()
-	.AddInfrastructure(builder.Configuration);
+//! Logging
+Log.Logger = new LoggerConfiguration()
+	.ReadFrom.Configuration(builder.Configuration).CreateLogger();
+
+builder.Services.AddSerilog();
 
 //! API
 builder.Services.AddControllersWithViews();
@@ -18,11 +19,26 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+	var provider = scope.ServiceProvider;
+
+	var context = provider.GetRequiredService<ApplicationDbContext>();
+
+	if (context.Database.GetMigrations().Any())
+	{
+		context.Database.Migrate();
+	}
 }
 
 app.UseHttpsRedirection();
+
+app.UseMiddleware<RequestLogContextMiddleware>();
+app.UseSerilogRequestLogging();
 
 app.UseStaticFiles();
 
